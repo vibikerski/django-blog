14d2ebd8118ce5be89b0810b477bc7af89ff052b
vibikerski <vibikerski@gmail.com>
2024-02-10 12:08:20 +0100

    style

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 from datetime import datetime, timedelta
 
+
 class Author(models.Model):
     name = models.CharField(max_length=255)
     bio = models.TextField()
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     published_date = models.DateTimeField()
-    
+
     author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.title
-    
+
     def published_recently(self):
         return self.published_date + timedelta(days=7) > datetime.now()
 
@@ -27,6 +29,6 @@ class Comment(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField()
-    
+
     def __str__(self):
         return self.text[:20] if len(self.text) > 20 else self.text
--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,45 @@
 from django.shortcuts import render
 from blog.models import Post, Author, Comment
 
+
 def get_all_posts(request):
     posts = Post.objects.all()
     context = {
         "post_list": posts,
     }
-    
+
     return render(
         request,
         'blog/posts_template.html',
         context
     )
 
+
 def get_post_by_id(request, post_id):
     post = Post.objects.get(id=post_id)
-    comments = Comment.objects.filter(post = post)
+    comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "author": post.author,
         "comments": comments
     }
-    
+
     return render(
         request,
         'blog/post_template.html',
         context
     )
 
+
 def get_posts_by_author(request, author_id):
     author = Author.objects.get(id=author_id)
     posts = Post.objects.filter(author=author)
-    
+
     context = {
         "post_list": posts,
         "author": author,
     }
-    
+
     return render(
         request,
         'blog/post_by_author_template.html',
